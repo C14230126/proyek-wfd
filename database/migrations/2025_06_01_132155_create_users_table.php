7e909671051d85d2491c222acbd5484c53de2553
Ju-Tzy <juan.m.davidson@gmail.com>
2025-06-01 21:38:12 +0700

    migration table users

---
+++ database/migrations/2025_06_01_132155_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->string('name', 30);
+            $table->string('nrp', 20)->nullable();
+            $table->string('jurusan', 100)->nullable();
+            $table->string('nip', 20)->nullable();
+            $table->string('email', 255)->unique();
+            $table->string('password', 255);
+            $table->string('no_hp', 20);
+            $table->enum('status', ['Requesting', 'Accepted']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
