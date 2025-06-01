89be8e2c48b5aed681a437fbb8e518f4596c96b2
Ju-Tzy <juan.m.davidson@gmail.com>
2025-06-01 21:40:24 +0700

    migration table barangs

---
+++ database/migrations/2025_06_01_133226_create_barangs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->id('barang_id');
+            $table->foreignId('kategori_id')->nullable()->constrained('kategori')->onDelete('set null');
+            $table->string('item', 255);
+            $table->integer('jumlah_unit');
+            $table->string('lokasi', 255)->nullable(); // barang ini di uppk mana
+            $table->enum('status', ['Borrowed', 'Returned'])->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barangs');
+    }
+};
