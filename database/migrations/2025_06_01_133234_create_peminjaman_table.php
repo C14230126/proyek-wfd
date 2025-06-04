9771f3da3c9fc69c43f91c2d25602b0ff61cd6f6
Ju-Tzy <juan.m.davidson@gmail.com>
2025-06-04 23:10:36 +0700

    ralat migration peminjaman

--- database/migrations/2025_06_01_133234_create_peminjaman_table.php
+++ database/migrations/2025_06_01_133234_create_peminjaman_table.php
@@ -12,13 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('peminjaman', function (Blueprint $table) {
-            $table->id('peminjaman_id');
+            $table->id();
             $table->unsignedBigInteger('barang_id');
-            $table->foreign('barang_id')->references('barang_id')->on('barangs')->onDelete('cascade');
+            $table->foreign('barang_id')->references('id')->on('barangs')->onDelete('cascade');
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->unsignedBigInteger('admin_id');
-            $table->foreign('admin_id')->references('user_id')->on('users')->onDelete('cascade');
+            $table->foreign('admin_id')->references('id')->on('users')->onDelete('cascade');
             $table->integer('quantitas');
             $table->string('nama_acara', 255);
             $table->string('lokasi_acara', 255);
