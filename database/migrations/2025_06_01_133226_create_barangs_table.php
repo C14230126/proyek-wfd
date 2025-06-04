f6ec37294b2d413affe4bdba7ae146b4f4c4b8b0
Ju-Tzy <juan.m.davidson@gmail.com>
2025-06-04 23:09:51 +0700

    ralat migration barangs

--- database/migrations/2025_06_01_133226_create_barangs_table.php
+++ database/migrations/2025_06_01_133226_create_barangs_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('barangs', function (Blueprint $table) {
-            $table->id('barang_id');
+            $table->id();
             $table->unsignedBigInteger('kategori_id')->nullable();
-            $table->foreign('kategori_id')->references('kategori_id')->on('kategori')->onDelete('set null');
+            $table->foreign('kategori_id')->references('id')->on('kategori')->onDelete('set null');
             $table->string('item', 255);
             $table->integer('jumlah_unit');
             $table->string('lokasi', 255)->nullable(); // barang ini di uppk mana
