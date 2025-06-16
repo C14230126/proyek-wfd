eaad02697a0456e284dfdf5df4c7b84f9288472f
C14230126 <c14230126@john.petra.ac.id>
2025-06-16 12:32:12 +0700

    nambah page listuser kapten

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,7 +30,7 @@ class User extends Authenticatable
     ];
     public function role()
     {
-        return $this->hasMany(Roles::class);
+        return $this->hasOne(Roles::class);
     }
 
     public function peminjaman()
@@ -80,4 +80,5 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
 }
