fdb7caef061f31ec7c7b6e93b0470c27f5492599
Ju-Tzy <juan.m.davidson@gmail.com>
2025-06-17 12:00:17 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,16 +12,16 @@ class UserController extends Controller
         $users = User::whereHas('role', function ($query) {
             $query->where('role', '!=', 'admin');
         })->with('role')->get();
-    
+
         return view('listusers', compact('users'));
     }
 
     public function destroy($id)
     {
         $user = \App\Models\User::findOrFail($id);
+        $userName = $user->name;
         $user->delete();
 
-        return redirect()->route('listusers')->with('success', 'User berhasil dihapus.');
+        return redirect()->route('listusers.index')->with('success', 'User ' . $userName . ' berhasil dihapus.');
     }
-
 }
