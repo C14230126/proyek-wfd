65170cd8c3c6c16f26b54121784eda4236a9b636
Klassikboii <reynard.sebastian.h@gmail.com>
2025-06-17 18:28:10 +0700

    Update User.php --> role() jadi hasOne

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,7 +32,7 @@ class User extends Authenticatable
     ];
     public function role()
     {
-        return $this->hasMany(Roles::class);
+        return $this->hasOne(Roles::class);
     }
 
     public function peminjaman()
