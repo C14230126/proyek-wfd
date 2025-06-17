32daef063e48c5d7f738185c109af3e64e404af3
Ju-Tzy <juan.m.davidson@gmail.com>
2025-06-17 11:16:09 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,13 +23,13 @@ class AuthController extends Controller
         return redirect()->route('login')->with('success', 'Akun berhasil dibuat.');
     }
 
-        //$prefix = substr($validated['nrp'], 0, 2);
-        //$jurusan = match($prefix) {
-            //'11' => 'Informatika',
-            //'22' => 'Sistem Informasi',
-            //'33' => 'Desain Komunikasi Visual',
-            //default => 'Tidak Dikenal',
-        //};
+    //$prefix = substr($validated['nrp'], 0, 2);
+    //$jurusan = match($prefix) {
+    //'11' => 'Informatika',
+    //'22' => 'Sistem Informasi',
+    //'33' => 'Desain Komunikasi Visual',
+    //default => 'Tidak Dikenal',
+    //};
 
     //}
 
@@ -44,27 +44,27 @@ class AuthController extends Controller
     }
     public function authenticate(Request $request)
     {
-    $credentials = $request->validate([
-        'email' => ['required', 'email'],
-        'password' => ['required'],
-    ]);
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
 
-    if (Auth::attempt($credentials)) {
-        $request->session()->regenerate(); // untuk keamanan sesi
-        return redirect()->intended('/'); // redirect ke halaman utama
-    }
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate(); // untuk keamanan sesi
+            return redirect()->intended('/'); // redirect ke halaman utama
+        }
 
-    return back()->withErrors([
-        'email' => 'Email atau password salah.',
-    ])->onlyInput('email');
+        return back()->withErrors([
+            'email' => 'Email atau password salah.',
+        ])->onlyInput('email')
+            ->with('error', 'Login gagal! Mohon periksa kembali email/password Anda.');
     }
     public function logout(Request $request)
     {
-    Auth::logout();
-    $request->session()->invalidate();
-    $request->session()->regenerateToken();
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
 
-    return redirect('/login');
+        return redirect('/login');
     }
-
 }
