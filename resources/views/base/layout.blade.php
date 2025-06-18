52fc8194c96e75cd207faf833642e220689f6446
Ju-Tzy <juan.m.davidson@gmail.com>
2025-06-18 21:43:39 +0700

    Update layout.blade.php

--- resources/views/base/layout.blade.php
+++ resources/views/base/layout.blade.php
@@ -15,7 +15,7 @@
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
-    background-attachment: fixed; /* ✅ KUNCI: agar gambar tetap diam */
+    background-attachment: fixed;
   }
 </style>
 
